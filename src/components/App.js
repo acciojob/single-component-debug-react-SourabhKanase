b151e72ef82a26d45a5194bdb0a46f2d8841ae73
SourabhKanase <141937826+SourabhKanase@users.noreply.github.com>
2023-12-24 11:34:28 +0000

    a

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from "react";
 
 let App=()=>{
     return (
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <div id="main">
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>
     )
 }
 export default App;
